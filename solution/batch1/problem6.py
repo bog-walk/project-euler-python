725e8550aaa4682458d3d97703024d946ddf6524
Bog Walk <bogwalk.brackish@gmail.com>
2021-09-07 15:24:41 -0400

    Write solution for Problem 6 & test cases.

---
+++ solution/batch1/problem6.py
@@ -0,0 +1,31 @@
+""" Problem 6: Sum Square Difference
+
+https://projecteuler.net/problem=6
+
+Goal: Find the absolute difference between the sum of the squares of &
+the square of the sum of the first N natural numbers.
+
+Constraints: 1 <= N <= 10^4
+
+e.g.: N = 3 -> {1,2,3}
+      sum of squares = {1,4,9} = 14
+      square of sum = 6^2 = 36
+      diff = |14 - 36| = 22
+"""
+
+
+def gaussian_sum(n):
+    return n * (n + 1) // 2
+
+
+def sum_square_diff(n):
+    """
+    Sum of squares is based on the assumption that:
+    f(n) = an^3 + bn^2 + cn + d, with
+    f(0) = 0, f(1) = 1, f(2) = 5, f(3) = 14; then
+    f(n) = 1/6(2n^3 + 3n^2 + n),
+    f(n) = n/6(2n + 1)(n + 1)
+    """
+    sum_of_squares = n * (2 * n + 1) * (n + 1) // 6
+    square_of_sum = pow(gaussian_sum(n), 2)
+    return square_of_sum - sum_of_squares
---
+++ test/batch1/test_problem6.py
@@ -0,0 +1,29 @@
+import unittest
+from solution.batch1.problem6 import sum_square_diff
+
+
+class SumSquareDifference(unittest.TestCase):
+    def test_sum_square_diff_lower_constraints(self):
+        test_n = [1, 2, 3]
+        expected = [0, 4, 22]
+        for index, n in enumerate(test_n):
+            self.assertEqual(expected[index], sum_square_diff(n))
+
+    def test_sum_square_diff_normal_values(self):
+        test_n = [10, 51, 100]
+        expected = [2640, 1712750, 25164150]
+        for index, n in enumerate(test_n):
+            self.assertEqual(expected[index], sum_square_diff(n))
+
+    def test_sum_square_diff_of_range_large_values(self):
+        test_n = [2256, 7000]
+        expected = [6477756566600, 600307154415500]
+        for index, n in enumerate(test_n):
+            self.assertEqual(expected[index], sum_square_diff(n))
+
+    def test_sum_square_diff_of_range_upper_constraint(self):
+        self.assertEqual(2500166641665000, sum_square_diff(10000))
+
+
+if __name__ == '__main__':
+    unittest.main()
