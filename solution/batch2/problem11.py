173556da9c10bc7bd64f89353539bae5d2922e12
Bog Walk <bogwalk.brackish@gmail.com>
2021-09-13 21:26:49 -0400

    Add solution for Problem 11 that doesn't use numpy import.
    Add test cases for Problem 11.

--- solution/batch2/problem11.py
+++ solution/batch2/problem11.py
@@ -13,3 +13,65 @@ e.g.: 1 1 1 1
       1 1 1 1
       column {1,2,3,1} = 6
 """
+from math import prod
+
+
+def transpose_grid(grid):
+    t_rows = len(grid[0])
+    t_cols = len(grid)
+    return [[grid[c][r] for c in range(t_cols)] for r in range(t_rows)]
+
+
+def rotate_grid(grid):
+    t_rows = len(grid[0])
+    t_cols = len(grid)
+    return [[grid[c][t_cols - r - 1] for c in range(t_cols)] for r in range(t_rows)]
+
+
+def largest_row_product(grid):
+    largest = 0
+    for row in grid:
+        for col in range(len(row) - 3):
+            product = prod(row[col:col + 4])
+            if product > largest:
+                largest = product
+    return largest
+
+
+def largest_col_product(grid):
+    t_grid = transpose_grid(grid)
+    return largest_row_product(t_grid)
+
+
+def largest_diagonal_product(grid):
+    t_grid = transpose_grid(grid)
+    return max(
+        largest_leading_diag(grid),
+        largest_leading_diag(t_grid),
+        largest_counter_diag(grid),
+        largest_counter_diag(t_grid)
+    )
+
+
+def largest_leading_diag(grid):
+    largest = 0
+    for row in range(len(grid) - 3):
+        for col in range(len(grid) - 3 - row):
+            diag = [grid[row][col], grid[row + 1][col + 1], grid[row + 2][col + 2], grid[row + 3][col + 3]]
+            product = prod(diag)
+            if product > largest:
+                largest = product
+    return largest
+
+
+def largest_counter_diag(grid):
+    r_grid = rotate_grid(grid)
+    return largest_leading_diag(r_grid)
+
+
+def largest_product_in_grid(grid):
+    return max(
+        largest_row_product(grid),
+        largest_col_product(grid),
+        largest_diagonal_product(grid)
+    )
--- test/batch2/test_problem11.py
+++ test/batch2/test_problem11.py
@@ -1,9 +1,11 @@
 import unittest
+from solution.batch2.problem11 import largest_row_product, largest_col_product, \
+    largest_diagonal_product, largest_product_in_grid
 
 
 def get_test_grid(filename):
     with open(filename) as gridFile:
-        grid = [row.strip().split() for row in gridFile]
+        grid = [list(map(lambda n: int(n), row.strip().split(" "))) for row in gridFile]
     return grid
 
 
@@ -16,8 +18,28 @@ class LargestProductInGrid(unittest.TestCase):
         cls.large_grid = get_test_grid("../resources/largest_product_in_grid_20by20")
 
     def test_grid_setup(self):
-        print(type(self.small_grid[0]))
-        self.assertEqual(4, len(self.small_grid))
+        self.assertEqual(20, len(self.large_grid))
+        self.assertEqual(20, len(self.large_grid[0]))
+        self.assertEqual(8, self.large_grid[0][0])
+        self.assertEqual(48, self.large_grid[19][19])
+        self.assertEqual(72, self.large_grid[15][7])
+
+    def test_largest_row_product(self):
+        self.assertEqual(3, largest_row_product(self.small_grid))
+        self.assertEqual(15, largest_row_product(self.mid_grid))
+
+    def test_largest_col_product(self):
+        self.assertEqual(6, largest_col_product(self.small_grid))
+        self.assertEqual(5, largest_col_product(self.mid_grid))
+
+    def test_largest_diagonal_product(self):
+        self.assertEqual(3, largest_diagonal_product(self.small_grid))
+        self.assertEqual(1, largest_diagonal_product(self.mid_grid))
+
+    def test_largest_product_in_grid(self):
+        self.assertEqual(6, largest_product_in_grid(self.small_grid))
+        self.assertEqual(15, largest_product_in_grid(self.mid_grid))
+        self.assertEqual(70600674, largest_product_in_grid(self.large_grid))
 
 
 if __name__ == '__main__':
