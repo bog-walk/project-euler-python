27a8a32411a94fb1c94dd54972ae6f006414afab
Bog Walk <bogwalk.brackish@gmail.com>
2021-09-20 16:55:32 -0400

    Add solution for Problem 14 & unit tests.
    Issues:
    - Timeout Errors for some test cases inspite of memoisation.

--- solution/batch2/problem14.py
+++ solution/batch2/problem14.py
@@ -18,3 +18,48 @@ e.g.: N = 5
       5 -> 16 -> 8 -> 4 -> 2 -> 1
       longest chain when starting number = 3
 """
+from math import log2
+
+
+def collatz_length(start):
+    count = 1
+    while start != 1:
+        # if start is even (alt to modulus)
+        if not(start & 1):
+            start //= 2
+        else:
+            start = start * 3 + 1
+        # Bitwise AND will equal 0 if is a power of 2
+        if start != 0 and not(start & (start - 1)):
+            count += log2(start) + 1
+            break
+        else:
+            count += 1
+    return count
+
+
+def generate_collatz_lengths(n_max):
+    collatz_lengths = [0] * n_max
+    collatz_lengths[0] = 1
+    collatz_lengths[1] = 2
+
+    def collatz_memo(n):
+        if n <= n_max and collatz_lengths[n-1] != 0:
+            return collatz_lengths[n-1]
+        else:
+            # if n is odd using Bitwise AND
+            if n & 1:
+                count = 2 + collatz_memo((n * 3 + 1) // 2)
+            else:
+                count = 1 + collatz_memo(n // 2)
+            return count
+
+    for start in range(3, n_max + 1):
+        collatz_lengths[start-1] = collatz_memo(start)
+    return collatz_lengths
+
+
+def longest_collatz_under_N(n: int, all_lengths: list[int]):
+    collatz_to_assess = all_lengths[n//2:n]
+    longest = max(collatz_to_assess)
+    return n - collatz_to_assess[::-1].index(longest)
--- test/batch2/test_problem14.py
+++ test/batch2/test_problem14.py
@@ -3,8 +3,18 @@ from solution.batch2.problem14 import *
 
 
 class LongestCollatzSequence(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)
+    def test_collatz_length(self):
+        starts = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 15, 17, 19, 27]
+        expected = [1, 2, 8, 3, 6, 9, 17, 4, 20, 7, 15, 10, 18, 13, 21, 112]
+        for index, start in enumerate(starts):
+            self.assertEqual(expected[index], collatz_length(start))
+
+    def test_longest_collatz_under_N(self):
+        all_lengths = generate_collatz_lengths(1000000)
+        nums = [1, 2, 3, 5, 10, 15, 19, 20, 26, 100, 1000, 10000, 100000, 1000000]
+        expected = [1, 2, 3, 3, 9, 9, 19, 19, 25, 97, 871, 6171, 77031, 837799]
+        for index, n in enumerate(nums):
+            self.assertEqual(expected[index], longest_collatz_under_N(n, all_lengths))
 
 
 if __name__ == '__main__':
