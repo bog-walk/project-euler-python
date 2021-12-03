14539d5f4f09fa7e9f4bd4be36467d743f90b0ed
Bog Walk <bogwalk.brackish@gmail.com>
2021-12-03 17:16:36 -0500

    Add solutions for Problem 31 & unit tests.
    Issues:
    - Bug in recursive function.

---
+++ solution/batch4/problem31.py
@@ -0,0 +1,77 @@
+""" Problem 31: Coin Sums
+
+https://projecteuler.net/problem=31
+
+Goal: Count the number of ways (mod 1e9 + 7) that N pence
+can be made using any combination of English coins.
+
+Constraints: 1 <= N <= 1e5
+
+English currency: There are 8 types of coins in circulation ->
+{1p, 2p, 5p, 10p, 20p, 50p, 1 pound (= 100p), 2 pound (= 200p)}.
+
+e.g.: N = 5 (i.e. goal is 5 pence in coins)
+      combos = [{5p}, {2p, 2p, 1p}, {2p, 1p, 1p, 1p}, {1p, 1p, 1p, 1p, 1p}]
+      count = 4
+"""
+coins = [1, 2, 5, 10, 20, 50, 100, 200]
+
+
+def count_coin_combos_recursive(n, coin=7):
+    recursive_cache = [[0] * 8] * (n + 1)
+
+    def recursive_combos(num, coin_index):
+        """
+        Recursive solution optimised with memoisation using top-down approach.
+        Repeatedly subtract each coin value from the target & sum combos previously
+        calculated for smaller targets.
+
+        :param [coin] Index of coin value from [coins] above. Default is the
+        largest coin available (2 pounds).
+        This parameter allows flexibility in method purpose ->
+        e.g. Could find combos for making 10p using 2p (& lower coins) = 6,
+        instead of making 10p using all possible coins = 11 combos.
+
+        SPEED:  for N = 1e5
+        """
+        if coin_index < 1:
+            print("Entered base")
+            return 1
+        if recursive_cache[num][coin_index] > 0:
+            print("Entered cache")
+            return recursive_cache[num][coin_index]
+        target = num
+        combos = 0
+        while target >= 0:
+            print(f"Recursion target={target} coin={coin_index} combos={combos}")
+            combos += recursive_combos(target, coin_index - 1)
+            target -= coins[coin_index]
+            print(f"Target={target}")
+        recursive_cache[num][coin_index] = combos
+        return combos
+
+    return recursive_combos(n, coin)
+
+
+def count_coin_combos(n):
+    """
+    Bottom-up approach that determines a target's combo based on:
+    - the previous combo calculated for the coin with a smaller target, &
+    - the previous combo calculated for a coin of lesser value.
+
+    SPEED (BEST):  for N = 1e5
+    Better performance due less expensive loops (vs recursive function calls)
+    & use of less memory with better cache-access.
+    """
+    combos_by_coin = [0] * (n + 1)
+    # Base case for when 0p is needed
+    combos_by_coin[0] = 1
+    for coin in coins:
+        for i in range(coin, n + 1):
+            combos_by_coin[i] += combos_by_coin[i - coin]
+    return combos_by_coin[n] % 1_000_000_007
+
+
+if __name__ == '__main__':
+    print(count_coin_combos_recursive(5))
+
---
+++ test/batch4/test_problem31.py
@@ -0,0 +1,42 @@
+import unittest
+from time import perf_counter_ns
+from solution.batch4.problem31 import *
+
+
+class CoinSums(unittest.TestCase):
+    def test_count_coin_combos_low_n(self):
+        expected = [1, 2, 2, 3, 4, 5, 6, 7, 8, 11]
+        for n in range(10):
+            # self.assertEqual(expected[n], count_coin_combos(n + 1))
+            self.assertEqual(expected[n], count_coin_combos_recursive(n + 1))
+
+    def test_count_coin_combos_mid_n(self):
+        nums = [15, 20, 50, 200, 500]
+        expected = [22, 41, 451, 73682, 6295434]
+        for i, n in enumerate(nums):
+            # self.assertEqual(expected[i], count_coin_combos(n))
+            self.assertEqual(expected[i], count_coin_combos_recursive(n))
+
+    def test_count_coin_combos_high_n(self):
+        n = 10000
+        expected = 296710490
+        # self.assertEqual(expected, count_coin_combos(n))
+        self.assertEqual(expected, count_coin_combos_recursive(n))
+
+    def test_count_coin_combos_speed(self):
+        n = 100000
+        solutions = [count_coin_combos_recursive, count_coin_combos]
+        starts = []
+        stops = []
+        answers = []
+        for solution in solutions:
+            starts.append(perf_counter_ns())
+            answers.append(solution(n))
+            stops.append(perf_counter_ns())
+        print(f"Recursive solution took: {stops[0] - starts[0]}ns\n"
+              f"Improved solution took: {stops[1] - starts[1]}ns\n")
+        self.assertEqual(answers[0], answers[1])
+
+
+if __name__ == '__main__':
+    unittest.main()
