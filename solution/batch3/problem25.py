11c096b4851017f5b6ed5eab421f70a449e086dd
Bog Walk <bogwalk.brackish@gmail.com>
2021-12-03 15:02:13 -0500

    Add alternate solutions & speed tests for Problem 25.

--- solution/batch3/problem25.py
+++ solution/batch3/problem25.py
@@ -10,10 +10,14 @@ e.g.: N = 3
       Fibonacci sequence = {0,1,1,2,3,5,8,13,21,34,55,89,144}
       first term with 3 digits = F(12)
 """
+from math import ceil, log10
 
 
 def n_digit_fib_terms(max_digits):
     """
+    SPEED (BEST for N<= 10): 0.0287ms for N = 10
+    4590ms (compared to <1ms with inverted formula) for N = 5000
+
     :return [list] of the first Fibonacci terms to have (index + 2) digits.
     """
     term = 7
@@ -30,3 +34,61 @@ def n_digit_fib_terms(max_digits):
             terms[digits - 2] = term
             digits += 1
     return terms
+
+
+def nth_fib_golden(n):
+    """
+    The golden ratio provides an alternative to iteration & is based on
+    the closed-form formula:
+    Fn = (Phi^n - Psi^n) / sqrt(5),
+    with Phi = (1 + sqrt(5)) / 2 ~= 1.61803... & Psi = -Phi^-1
+
+    Rounding, using the nearest integer function, reduces the formula to:
+    Fn = [Phi^n / sqrt(5)], where n >= 0.
+    Truncation, using the floor function, would result instead in:
+    Fn = [(Phi^n / sqrt(5)) + 0.5], where n >= 0.
+
+    :return the nth Fibonacci sequence number.
+    """
+    phi = (1 + 5 ** 0.5) / 2
+    return round(phi ** n / 5 ** 0.5)
+
+
+def n_digit_fib_term_using_golden(n):
+    """
+    Iterative solution uses Golden Ratio to calculate the Fibonacci
+    sequence numbers.
+
+    SPEED: 0.0406ms for N = 10
+    Significantly slower execution from N > 10, due to exponential
+    need to calculate Phi^N.
+
+    :return first Fibonacci term to have N digits.
+    """
+    term = 7
+    f_n = 13
+    # Pattern shows the amount of digits increases every 4th-5th term
+    step = 4
+    while len(str(f_n)) < n:
+        term += step
+        f_n = nth_fib_golden(term)
+        while len(str(f_n)) < n:
+            term += 1
+            f_n = nth_fib_golden(term)
+    return term
+
+
+def n_digit_fib_term_by_digits_golden(n):
+    """
+    O(n) solution based on the closed-form golden ratio formula shown
+    previously being inverted to calculate log10 of each Fibonacci number,
+    thereby returning the term that has the required amount of digits,
+    without the need to iterate.
+
+    SPEED: 39.7731ms for N = 10
+    (BEST for N > 10) 0.0837ms for N = 5000
+
+    :return first Fibonacci term to have N digits.
+    """
+    phi = (1 + 5 ** 0.5) / 2
+    return int(ceil((n - 1 + log10(5) / 2) / log10(phi)))
--- test/batch3/test_problem25.py
+++ test/batch3/test_problem25.py
@@ -1,14 +1,71 @@
 import unittest
-from solution.batch3.problem25 import n_digit_fib_terms
+from time import perf_counter_ns
+from solution.batch3.problem25 import *
 
 
 class NDigitFibonacciNumber(unittest.TestCase):
+
     def test_n_digit_fib_terms(self):
         all_terms = n_digit_fib_terms(5000)
         digits = [2, 3, 4, 9, 1000, 5000]
         expected = [7, 12, 17, 40, 4782, 23922]
         for index, digit in enumerate(digits):
             self.assertEqual(expected[index], all_terms[digit - 2])
+            self.assertEqual(expected[index], n_digit_fib_term_by_digits_golden(digit))
+
+    def test_nth_fib_golden(self):
+        expected = [
+            0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597
+        ]
+        for n in range(18):
+            self.assertEqual(expected[n], nth_fib_golden(n))
+
+    def test_n_digit_fib_term_using_golden(self):
+        expected = [7, 12, 17, 21, 26, 31, 36, 40]
+        for n in range(2, 10):
+            self.assertEqual(expected[n - 2], n_digit_fib_term_using_golden(n))
+
+    def test_n_digit_fib_term_speed_low_n(self):
+        n = 10
+        expected = 45
+        solutions = [
+            n_digit_fib_terms,
+            n_digit_fib_term_using_golden,
+            n_digit_fib_term_by_digits_golden
+        ]
+        starts = []
+        stops = []
+        for i in range(3):
+            starts.append(perf_counter_ns())
+            ans = solutions[i](n)
+            if i == 0:
+                self.assertEqual(expected, ans[n - 2])
+            else:
+                self.assertEqual(expected, ans)
+            stops.append(perf_counter_ns())
+        print(f"Brute solution took: {stops[0] - starts[0]}ns\n"
+              f"Golden iteration took: {stops[1] - starts[1]}ns\n"
+              f"Golden by digits took: {stops[2] - starts[2]}ns\n")
+
+    def test_n_digit_fib_term_speed_high_n(self):
+        n = 5000
+        expected = 23922
+        solutions = [
+            n_digit_fib_terms,
+            n_digit_fib_term_by_digits_golden
+        ]
+        starts = []
+        stops = []
+        for i in range(2):
+            starts.append(perf_counter_ns())
+            ans = solutions[i](n)
+            if i == 0:
+                self.assertEqual(expected, ans[n - 2])
+            else:
+                self.assertEqual(expected, ans)
+            stops.append(perf_counter_ns())
+        print(f"Brute solution took: {(stops[0] - starts[0]) // 1_000_000}ms\n"
+              f"Golden by digits took: {stops[1] - starts[1]}ns\n")
 
 
 if __name__ == '__main__':
