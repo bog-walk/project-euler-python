c1d69ca610841c78db57063cbdd7a2108fe4c9e8
Bog Walk <bogwalk.brackish@gmail.com>
2021-12-09 10:46:14 -0500

    Add PE specific solution for Problem 38 & update unit tests.

--- solution/batch4/problem38.py
+++ solution/batch4/problem38.py
@@ -26,13 +26,15 @@ def find_pandigital_multipliers(n, k):
     """
     multipliers = []
     limit = min(n, 10000)
-    for num in range(9000, limit):
+    for num in range(2, limit):
         concat = ""
         i = 1
         while len(concat) < k:
             product = str(num * i)
+            # ensure result only 1 to k pandigital
             if any(ch == "0" or ch > str(k) or ch in concat for ch in product):
                 break
+            # avoid products that contain duplicate digits
             if len(product) != len(set(product)):
                 break
             concat += product
@@ -40,3 +42,39 @@ def find_pandigital_multipliers(n, k):
         if len(concat) == k:
             multipliers.append(num)
     return multipliers
+
+
+def largest_9_pandigital():
+    """
+    Project Euler specific implementation that finds the largest
+    1 to 9 pandigital number that can be formed as a concatenated product.
+    Solution is based on the following:
+
+    - Since the multiplier must at minimum be multiplied by both 1 & 2, it
+    cannot be larger than 4 digits to ensure product is only 9 digits.
+
+    - The default largest would be M = 9 * (1, 2, 3, 4, 5) = 918273645, so
+    M must begin with the digit 9.
+
+    - The 2-digit minimum (M = 91) would result in either an 8-/11-digit
+    product once multiplied by 3 and 4. The 3-digit minimum (M = 912) would
+    result in either a 7-/11-digit product once multiplied by 2 and 3.
+
+    - So M must be a 4-digit number multiplied by 2 to get a 9-digit product
+    and at minimum will be 9182 (1st 4 digits of default) and at max 9876.
+
+    - Multiplying 9xxx by 2 will at minimum result in 18xxx, always
+    generating a new digit 1, so M cannot itself contain the digit 1, setting
+    the new minimum to 9234.
+
+    - Lastly, multiplying (98xx downTo 95xx) by 2 will at minimum result in 19xxx,
+    always generating another digit 9, so M's 2nd digit must be < 5, setting
+    the new maximum to 9487.
+    """
+    pandigital = [str(d) for d in range(1, 10)]
+    largest = ""
+    for m in range(9487, 9234, -1):
+        largest = str(m) + str(m * 2)
+        if pandigital == sorted(largest):
+            break
+    return largest
--- test/batch4/test_problem38.py
+++ test/batch4/test_problem38.py
@@ -1,5 +1,5 @@
 import unittest
-from solution.batch4.problem38 import find_pandigital_multipliers
+from solution.batch4.problem38 import *
 
 
 class PandigitalMultiples(unittest.TestCase):
@@ -8,6 +8,25 @@ class PandigitalMultiples(unittest.TestCase):
         expected = [[18, 78], [9]]
         for k in range(8, 10):
             self.assertEqual(expected[k - 8], find_pandigital_multipliers(n, k))
+
+    def test_find_pandigital_multipliers_mid_N(self):
+        n = 1000
+        expected = [[18, 78], [9, 192, 219, 273, 327]]
+        for k in range(8, 10):
+            self.assertEqual(expected[k - 8], find_pandigital_multipliers(n, k))
+
+    def test_find_pandigital_multipliers_high_N(self):
+        n = 10000
+        expected = [
+            [18, 78, 1728, 1764, 1782, 1827, 2178, 2358, 2718, 2817, 3564, 3582, 4176, 4356],
+            [9, 192, 219, 273, 327, 6729, 6792, 6927, 7269, 7293, 7329, 7692, 7923, 7932, 9267, 9273, 9327]
+        ]
+        for k in range(8, 10):
+            self.assertEqual(expected[k - 8], find_pandigital_multipliers(n, k))
+
+    def test_largest_9_pandigital(self):
+        expected = "932718654"
+        self.assertEqual(expected, largest_9_pandigital())
 
 
 if __name__ == '__main__':
