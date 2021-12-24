87f968860f18f98b0983fc468aa5c46ffd464400
Bog Walk <bogwalk.brackish@gmail.com>
2021-12-23 22:33:39 -0400

    Add solutions & unit tests for Problem 46

--- solution/batch5/problem46.py
+++ solution/batch5/problem46.py
@@ -7,12 +7,12 @@ represented as proposed by Goldbach's Conjecture, detailed below.
 
 Constraints: 9 <= N < 5e5
 
-Goldbach's Proposed Conjecture: Every odd composite number can be written as
-the sum of a prime and twice a square. Proven to be FALSE.
+Goldbach's False Conjecture: Every odd composite number can be
+written as the sum of a prime and twice a square. Proven to be FALSE.
 e.g. 9 = 7 + 2 * 1^2
      15 = 7 + 2 * 2^2 or 13 + 2 * 1^2
-     21 = 3 + 2 * 3^2
-     25 = 7 + 2 * 3^2
+     21 = 3 + 2 * 3^2 or 13 + 2 * 2^2 or 19 + 2 * 1^2
+     25 = 7 + 2 * 3^2 or 17 + 2 * 2^2 or 23 + 2 * 1^2
      27 = 19 + 2 * 2^2
      33 = 31 + 2 * 1^2
 
@@ -21,12 +21,43 @@ e.g.: N = 9
       N = 15
       result = 2
 """
+from math import sqrt, floor
+from util.reusable import is_prime, prime_numbers
+
+
+def is_goldbach_2(composite, prime):
+    rep = sqrt((composite - prime) / 2)
+    return rep == floor(rep)
+
+
+def goldbach_repr(n):
+    """
+    :param n: An odd composite number (no in-built check
+    for primality).
+    """
+    primes = prime_numbers(n)[1:]
+    count = 0
+    for prime in primes:
+        if is_goldbach_2(n, prime):
+            count += 1
+    return count
 
 
 def smallest_failing_num():
     """
     Project Euler specific implementation that returns the smallest odd
-    composite that cannot be written, as proposed, as the sum of a prime and
-    twice a square.
+    composite that cannot be written, as proposed, as the sum of a prime
+    and twice a square.
     """
+    primes = prime_numbers(10000)[1:]
+    composite = 33  # starting point as provided in example
+    while True:
+        composite += 2
+        if is_prime(composite):
+            continue
+        for prime in primes:
+            if prime > composite:
+                return composite
+            if is_goldbach_2(composite, prime):
+                break
     return -1
--- test/batch5/test_problem46.py
+++ test/batch5/test_problem46.py
@@ -1,9 +1,33 @@
 import unittest
+from solution.batch5.problem46 import smallest_failing_num, goldbach_repr
 
 
 class GoldbachsOtherConjecture(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
+    def test_goldbach_repr_low(self):
+        odds = [9, 15, 21, 25, 27, 33]
+        expected = [1, 2, 3, 3, 1, 1]
+        for i, odd in enumerate(odds):
+            self.assertEqual(expected[i], goldbach_repr(odd))
+
+    def test_goldbach_repr_mid(self):
+        odds = [403, 695, 1599, 3393]
+        expected = [5, 2, 12, 10]
+        for i, odd in enumerate(odds):
+            self.assertEqual(expected[i], goldbach_repr(odd))
+
+    def test_goldbach_repr_high(self):
+        odds = [23851, 499999]
+        expected = [53, 156]
+        for i, odd in enumerate(odds):
+            self.assertEqual(expected[i], goldbach_repr(odd))
+
+    def test_smallest_failing_num(self):
+        expected = 5777
+        self.assertEqual(expected, smallest_failing_num())
+
+    def test_goldbach_repr_no_rep(self):
+        expected = 0
+        self.assertEqual(expected, goldbach_repr(5777))
 
 
 if __name__ == '__main__':
