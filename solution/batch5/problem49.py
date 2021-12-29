f75aa0e897c6f095b742f3257a5c1081a375a5ed
Bog Walk <bogwalk.brackish@gmail.com>
2021-12-29 14:17:38 -0400

    Fix alternate solution for Problem 49 to increase speed.

--- solution/batch5/problem49.py
+++ solution/batch5/problem49.py
@@ -17,14 +17,21 @@ from itertools import permutations
 from util.reusable import is_prime, prime_numbers
 
 
-def prime_perm_sequence(n, k):
+def prime_perm_sequence(n, k) -> list[list[int]]:
     """
-    Note that there are no arithmetic sequences that match these
-    properties with terms having less than 4 digits.
+    Solution uses itertools.permutations() to find all permutations
+    of a prime & filter potential candidates for a sequence. Will be
+    slower due to permutations being (re-)generated unnecessarily.
+
+    SPEED: 49.73s for N = 1e6, K = 3
     """
     primes = prime_numbers(n - 1)
     sequences = []
-    for first in primes[186:]:
+    for first in primes:
+        # there are no arithmetic sequences that match these
+        # properties with terms having less than 4 digits.
+        if first < 1117:
+            continue
         num_str = str(first)
         perms = sorted(list(
             filter(
@@ -45,46 +52,69 @@ def prime_perm_sequence(n, k):
                     constant = False
                     break
             if constant:
-                concat = "".join([str(first + (m * diff)) for m in range(k)])
+                concat = [first + (m * diff) for m in range(k)]
                 sequences.append(concat)
     return sequences
 
 
-def alt(n, k):
+def perm_id(n):
+    """
+    Generate a hash key for a prime number based on the
+    amount of repeated digits, represented as a numerical
+    version of an indexed RTL array.
+
+    e.g. 1487 -> 110010010 <- 4817
+         2214 -> 10210 <- 4212
+    """
+    p_id = 0
+    while n:
+        digit = n % 10
+        p_id += pow(10, digit)
+        n //= 10
+    return p_id
+
+
+def prime_perm_sequence_improved(n, k) -> list[list[int]]:
+    """
+    Solution optimised by using a helper function that maps
+    all primes with same type and amount of digits to a permutation id.
+    Then every list of primes that share a permutation id and has >= K
+    elements is iterated over to check for an arithmetic progression
+    sequence. Also eliminates need to check for primality by pre-
+    generating all primes with same number of digits.
+
+    SPEED (BETTER): 5.64s for N = 1e6, K = 3
+    """
     primes = prime_numbers(pow(10, len(str(n))) - 1)
-    prime_perms = dict()
-    for prime in primes[186:]:
-        if prime >= n:
-            break
-        if prime in prime_perms:
-            continue
-        perms = sorted([
-            int("".join(perm))
-            for perm in set(permutations(str(prime)))
-            if perm[0] != '0' and int("".join(perm)) in primes
-        ])
-        for i in range(len(perms) - k + 1):
-            if perms[i] >= n:
-                break
-            prime_perms[perms[i]] = perms[i+1:]
+    prime_perms: dict[int, list[int]] = dict()
     sequences = []
-    for prime, perms in sorted(prime_perms.items()):
-        if len(perms) >= k - 1:
-            for i in range(len(perms) - k + 2):
-                second = perms[i]
-                diff = second - prime
-                extra = second
-                constant = True
-                for _ in range(k - 2):
-                    extra += diff
-                    if extra not in perms[i + 1:]:
-                        constant = False
-                        break
-                if constant:
-                    concat = "".join([str(prime + (m * diff)) for m in range(k)])
-                    sequences.append(concat)
+    for prime in primes:
+        if prime < 1117:
+            continue
+        p_id = perm_id(prime)
+        prime_perms[p_id] = prime_perms.setdefault(p_id, []) + [prime]
+    for perms in prime_perms.values():
+        if len(perms) >= k:
+            for i in range(len(perms) - k + 1):
+                first = perms[i]
+                if first >= n:
+                    break
+                for j in range(i + 1, len(perms) - k + 2):
+                    diff = perms[j] - first
+                    constant = True
+                    for x in range(2, k):
+                        if first + (x * diff) not in perms[j + 1:]:
+                            constant = False
+                            break
+                    if constant:
+                        concat = [first + (m * diff) for m in range(k)]
+                        sequences.append(concat)
     return sequences
 
 
-if __name__ == '__main__':
-    print(len(prime_perm_sequence(1000000, 4)))
+def concat_prime_perms(n, k, improved: bool) -> list[str]:
+    if improved:
+        results = sorted(prime_perm_sequence_improved(n, k))
+    else:
+        results = prime_perm_sequence(n, k)
+    return ["".join(map(str, result)) for result in results]
--- test/batch5/test_problem49.py
+++ test/batch5/test_problem49.py
@@ -1,15 +1,18 @@
 import unittest
-from solution.batch5.problem49 import prime_perm_sequence
+from time import perf_counter
+from solution.batch5.problem49 import concat_prime_perms
 
 
 class PrimePermutations(unittest.TestCase):
-    def test_prime_perm_seq_4_digit(self):
+    def test_concat_prime_perms_4_digit(self):
         n = 10_000
         expected = ['148748178147', '296962999629']
-        self.assertListEqual(expected, prime_perm_sequence(n, k=3))
-        self.assertListEqual([], prime_perm_sequence(n, k=4))
+        self.assertListEqual(expected, concat_prime_perms(n, k=3, improved=False))
+        self.assertListEqual([], concat_prime_perms(n, k=4, improved=False))
+        self.assertListEqual(expected, concat_prime_perms(n, k=3, improved=True))
+        self.assertListEqual([], concat_prime_perms(n, k=4, improved=True))
 
-    def test_prime_perm_seq_5_digit_low(self):
+    def test_concat_prime_perms_5_digit_low(self):
         n = 20_000
         expected = [
             '148748178147', '296962999629', '114831481318143', '114974171971941',
@@ -17,18 +20,30 @@ class PrimePermutations(unittest.TestCase):
             '148214812181421', '148313148148131', '148974718979481', '185035180385103',
             '185935189385193', '195433549151439'
         ]
-        self.assertListEqual(expected, prime_perm_sequence(n, k=3))
-        self.assertListEqual([], prime_perm_sequence(n, k=4))
+        self.assertListEqual(expected, concat_prime_perms(n, k=3, improved=False))
+        self.assertListEqual(expected, concat_prime_perms(n, k=3, improved=True))
+        self.assertListEqual([], concat_prime_perms(n, k=4, improved=False))
+        self.assertListEqual([], concat_prime_perms(n, k=4, improved=True))
 
-    def test_prime_perm_seq_5_digit_high(self):
+    def test_concat_prime_perms_5_digit_high(self):
         n = 100_000
-        self.assertEqual(55, len(prime_perm_sequence(n, k=3)))
+        self.assertEqual(55, len(concat_prime_perms(n, k=3, improved=False)))
+        self.assertEqual(55, len(concat_prime_perms(n, k=3, improved=True)))
         expected = ['83987889379388798837']
-        self.assertListEqual(expected, prime_perm_sequence(n, k=4))
+        self.assertListEqual(expected, concat_prime_perms(n, k=4, improved=False))
+        self.assertListEqual(expected, concat_prime_perms(n, k=4, improved=True))
 
-    def test_prime_perm_seq_6_digit(self):
+    def test_concat_prime_perms_speed(self):
         n = 1_000_000
-        self.assertEqual(15, len(prime_perm_sequence(n, k=4)))
+        expected = 883
+        builtin_start = perf_counter()
+        self.assertEqual(expected, len(concat_prime_perms(n, k=3, improved=False)))
+        builtin_stop = perf_counter()
+        improved_start = perf_counter()
+        self.assertEqual(expected, len(concat_prime_perms(n, k=3, improved=True)))
+        improved_stop = perf_counter()
+        print(f"Builtin solution took: {builtin_stop - builtin_start:0.2f}s\n" +
+              f"Improved solution took: {improved_stop - improved_start:0.2f}s\n")
 
 
 if __name__ == '__main__':
