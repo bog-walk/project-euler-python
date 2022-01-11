0df6f232ddc104a7162487f25ff8a54f6b555f96
Bog Walk <bogwalk.brackish@gmail.com>
2022-01-11 15:55:17 -0500

    Improve Problem 53 solution & add unit tests & speed test.

--- solution/batch6/problem53.py
+++ solution/batch6/problem53.py
@@ -2,38 +2,95 @@
 
 https://projecteuler.net/problem=53
 
-Goal: Count the values of n_C_r, for 1 <= n <= N, that are
+Goal: Count the values of C(n, r), for 1 <= n <= N, that are
 greater than K. Values do not have to be distinct.
 
 Constraints: 2 <= N <= 1000, 1 <= K <= 1e18
 
-Combinatorics: n_C_r = n! / (r! * (n - r)!), where r <= n.
-There are 10 combinations when 3 digits are chosen from 5 digits.
+Binomial Coefficient: C(n, r) = n! / (r! * (n - r)!), where r <= n.
+There are 10 combinations when 3 digits are chosen from 5 digits,
+with no repetition & order not mattering: C(5, 3) = 10.
 It is not until n = 23 that the amount of combinations first
-exceeds 1e6, namely 23_C_10 = 1_144_066.
+exceeds 1e6, namely C(23, 10) = 1_144_066.
 
 e.g.: N = 23, K = 1e6
       answer = 4
 """
 from math import factorial
 
-
+# Acceptable in PY due to overflow not being an issue, since
+# 13! overflows 32 bits and 21! overflows 64 bits and
+# 59! > 1e80 (postulated to be the number of particles in the universe).
 factorials = [factorial(i) for i in range(1001)]
 
 
-def get_combinatorics(n, r):
+def binomial_coefficient(n, r):
     return factorials[n] // (factorials[r] * factorials[n - r])
 
 
 def count_large_combinatorics(n, k):
+    """
+    Solution optimised based on the symmetry of Pascal's Triangle:
+
+    - C(n, 0) = 1 & C(n, 1) = n. k could be less than n, so must start
+    r-loop at 1.
+
+    - C(n, r) = C(n, n-r) & peaks for each row at the mid-point.
+
+    - So if C(n, r) > k, then all C(n, x) for x in [r+1, n-r] will also be > k.
+    Based on the incrementing row count (n + 1), this can be calculated as
+    n - 2 * r + 1, so the rest of the row values do not need to be also calculated.
+
+    - Starting from the bottom of the triangle & moving up, if no value in a row
+    is greater than k, then no row (of lesser n) will have valid values & the
+    outer loop can be broken.
+
+    SPEED: 0.0485s for N = 1000, K = 1000 repeated 10 times
+    """
     count = 0
+    not_found = False
     while n > 0:
-        for r in range(1, n):
-            if get_combinatorics(n, r) > k:
-                count += 1
+        for r in range(1, n // 2 + 1):
+            if binomial_coefficient(n, r) > k:
+                count += n - 2 * r + 1
+                break
+            if r == n // 2:
+                not_found = True
+        if not_found:
+            break
         n -= 1
     return count
 
 
-if __name__ == '__main__':
-    print(count_large_combinatorics(23, 1_000_000))
+def count_large_combinatorics_improved(n, k):
+    """
+    Solution improved by not depending on factorials to pre-compute the
+    binomial coefficient, thereby also needing types that can handle >64 bits.
+
+    Solution is still based on the symmetry of Pascal's Triangle & its
+    rules as detailed in the function above, with some additions:
+
+    - C(n, r+1) = C(n, r) * ((n-r) / (r+1)) and C(n-1, r) = C(n, r) * ((n-r) / n).
+    Movement through the triangle (bottom-up & only checking border values) mimics
+    that in the above function, but C(n, r) values when moving right in a row or up
+    a row are determined with these formulae, instead of factorials.
+
+    - Starting from the bottom of the triangle & moving up, if the value of r is
+    allowed to exceed its midline value, then it means no value > k was found and
+    the outer loop can be broken.
+
+    SPEED (BETTER): 0.0049s for N = 1000, K = 1000 repeated 10 times
+    """
+    count = 0
+    r, n_c_r = 0, 1  # start at left-most border
+    while r <= n // 2:
+        next_in_row = n_c_r * (n - r) // (r + 1)
+        if next_in_row > k:
+            # count formula differs from above function
+            # because r is 1 less than r in next_in_row
+            count += n - 2 * r - 1
+            n_c_r = n_c_r * (n - r) // n
+            n -= 1
+        else:
+            r, n_c_r = r + 1, next_in_row
+    return count
--- test/batch6/test_problem53.py
+++ test/batch6/test_problem53.py
@@ -1,13 +1,40 @@
 import unittest
 from util.tests.reusable import compare_speed_seconds
-from solution.batch6.problem53 import *
+from solution.batch6.problem53 import count_large_combinatorics, \
+    count_large_combinatorics_improved
 
 
 class CombinatoricSelections(unittest.TestCase):
-    def test_def(self):
-        n = 1
-        expected = 1
-        self.assertEqual(expected, n)
+    def test_count_large_combinatorics_lower_constraints(self):
+        k = 5
+        expected = [0, 0, 1, 3, 8, 14]
+        for n in range(2, 8):
+            self.assertEqual(expected[n - 2], count_large_combinatorics(n, k))
+            self.assertEqual(expected[n - 2], count_large_combinatorics_improved(n, k))
+
+    def test_count_large_combinatorics(self):
+        nums = [2, 23, 100, 1000]
+        k = 1_000_000
+        expected = [0, 4, 4075, 494_861]
+        for i, n in enumerate(nums):
+            self.assertEqual(expected[i], count_large_combinatorics(n, k))
+            self.assertEqual(expected[i], count_large_combinatorics_improved(n, k))
+
+    def test_count_large_combinatorics_upper_constraint(self):
+        n, k = 1000, 10_000_000_000
+        expected = 490_806
+        self.assertEqual(expected, count_large_combinatorics(n, k))
+        self.assertEqual(expected, count_large_combinatorics_improved(n, k))
+
+    def test_speed_compare(self):
+        n, k = 1000, 1000
+        expected = 497_376
+        solutions = {
+            "OG": [count_large_combinatorics, n, k],
+            "Improved": [count_large_combinatorics_improved, n, k]
+        }
+        results = compare_speed_seconds(solutions, repeat=10)
+        self.assertTrue(all([expected == actual for actual in results.values()]))
 
 
 if __name__ == '__main__':
